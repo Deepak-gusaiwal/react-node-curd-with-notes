02b4280ef429d2e8ecf3172aac00585f84335a1f
Deepak_Home <dgusaiwal537@gmail.com>
2024-08-07 17:12:26 +0530

    add api url directly in Axios.js

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { apiUrl } from "./env";
 console.log('api url is',apiUrl);
 export const Axios = axios.create({
-  baseURL: apiUrl,
+  baseURL: "https://react-node-curd-with-notes.onrender.com/api/v1/",
   withCredentials: true,
 });
