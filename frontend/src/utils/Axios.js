e24dcb3aed1ca0dc896b6d1ef598efc430ee218a
Deepak_Home <dgusaiwal537@gmail.com>
2024-08-07 18:07:34 +0530

    add frontend api url in .env file

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { apiUrl } from "./env";
-// console.log('api url is',apiUrl);
 export const Axios = axios.create({
-  baseURL: "https://react-node-curd-with-notes.onrender.com/api/v1/",
+  baseURL: apiUrl,
   withCredentials: true,
 });
