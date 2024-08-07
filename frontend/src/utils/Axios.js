2a9ce23bd7472e552803ebb173180373502e48d9
Deepak_Home <dgusaiwal537@gmail.com>
2024-08-07 17:24:19 +0530

    remove .env file from backend also and create anew .local.env

--- frontend/src/utils/Axios.js
+++ frontend/src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { apiUrl } from "./env";
-console.log('api url is',apiUrl);
+// console.log('api url is',apiUrl);
 export const Axios = axios.create({
   baseURL: "https://react-node-curd-with-notes.onrender.com/api/v1/",
   withCredentials: true,
